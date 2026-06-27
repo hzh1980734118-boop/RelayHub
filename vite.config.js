613773970c302e3f3e75d639f8efc5a3677fcb93
hzh1980734118-boop <hzh1980734118@gmail.com>
2026-06-27 19:40:30 +0800

    补充base配置，更新打包静态文件，上传完整项目配置

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  // 关键配置：仓库名称，前后斜杠必须带上
+  base: '/RelayHub/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
